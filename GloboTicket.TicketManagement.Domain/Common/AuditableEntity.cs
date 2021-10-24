e25708e6f340de5b03980f5c36745fdf6e140052
Enrique Beltran <cesar78200921@hotmail.com>
2021-10-24 12:33:30 -0400

    Add project files.

---
+++ GloboTicket.TicketManagement.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GloboTicket.TicketManagement.Domain.Common
+{
+    public class AuditableEntity
+    {
+        public string CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
---
+++ GloboTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Domain.Common;
+
+namespace GloboTicket.TicketManagement.Domain.Entities
+{
+    public class Category: AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+        
+    }
+}
